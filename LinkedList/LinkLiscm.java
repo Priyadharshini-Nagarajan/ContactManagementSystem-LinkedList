ad8e2483456aa475572a9e198ec77e51e1cc3955
Priyadharshini-Nagarajan <161636455+Priyadharshini-Nagarajan@users.noreply.github.com>
2024-03-21 19:33:16 +0530

    Add files via upload

--- LinkedList/LinkLiscm.java
+++ LinkedList/LinkLiscm.java
@@ -1,1 +1,59 @@
 
+    class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    private Node head;
+
+    public LinkedList() {
+        head = null;
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+        System.out.println("Added: " + data);
+    }
+
+    public void display() {
+        if (head == null) {
+            System.out.println("List is empty.");
+        } else {
+            Node temp = head;
+            System.out.print("List: ");
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+}
+
+public class LinkLiscm  {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(5);
+        list.add(10);
+        list.add(15);
+
+        list.display();
+    }
+}
+
